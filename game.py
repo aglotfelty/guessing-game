a4778f46f3c9aa3745da3328af66472f8d0bc55d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 15:44:04 -0800

    Added counter to track number of guesses

--- game.py
+++ game.py
@@ -5,17 +5,18 @@ player_name = raw_input("What is your name? ")
 
 print "%s, I'm thinking of a number between 1 and 100." % player_name
 
-random_number = randint(1, 101)
+random_number = randint(1, 100)
 print "Guess what number I am thinking: "
-print random_number
+counter = 0
 
 while True:
     guess = int(raw_input(">> "))
+    counter += 1
     if guess != random_number:
         if guess > random_number:
             print "Your guess is too high. Try again."
         else:
             print "Your number is too low. Try again."
     else:
-        print "Congratulation! You found my number!"
+        print "Congratulations! You found my number in %d guesses!" % counter
         break
