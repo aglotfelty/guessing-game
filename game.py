7c40bb005d0ab3f4973898ab77b588bb655b781e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 15:13:59 -0800

    added ability for user to guess a number

--- game.py
+++ game.py
@@ -6,3 +6,5 @@ player_name = raw_input("What is your name? ")
 print "%s, I'm thinking of a number between 1 and 100." % player_name
 
 random_number = randint(1, 101)
+print "Guess what number I am thinking: "
+guess = raw_input(">> ")
