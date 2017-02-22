621c6c905510feb0df754f4dc3d9bd4bd5687f04
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-21 17:33:20 -0800

    Updated try/except & allow users to play multiple rounds.

--- game.py
+++ game.py
@@ -2,17 +2,25 @@ from random import randint
 
 print "Hello player! Welcome to our guessing game."
 player_name = raw_input("What is your name? ")
-
 print "%s, I'm thinking of a number between 1 and 100." % player_name
+playing_game = True
+best_score = 0
 
-random_number = randint(1, 100)
-print "Guess what number I am thinking: "
-counter = 0
+while playing_game:   
+    print "Great! Let's play" 
+    random_number = randint(1, 100)
+    print random_number
+    print "Guess what number I am thinking: "
+    counter = 0
 
-while True:
-    counter += 1
-    try:
-        guess = int(raw_input(">> "))
+    while True:
+        counter += 1
+        try:
+            guess = int(raw_input(">> "))
+        except ValueError:
+            print "That is not a number! Try again."
+            continue
+
         if guess != random_number:
             if guess > 100 or guess < 1:
                 print "You idiot!! That number is not between 1 and 100. Try again."
@@ -23,26 +31,36 @@ while True:
         else:
             print "Congratulations! You found my number in %d guesses!" % counter
             break
-    except ValueError:
-        print "That is not a number!"
+    play_again = raw_input("Would you like to play again? (Yes/No) ")
+    if play_again == "Yes":
+        continue
+    else:
+        playing_game = False
+print "Thank you for playing our game!"
 
 
-# while True:
-#     while True:
-#         counter += 1
-#         try:
-#             guess = int(raw_input(">> "))
-#             break
-#         except ValueError:
-#             print "That is not a number! Try again."
-
-#     if guess != random_number:
-#         if guess > 100 or guess < 1:
-#             print "You idiot!! That number is not between 1 and 100. Try again."
-#         elif guess > random_number:
-#             print "Your guess is too high. Try again."
+# while playing_game:
+#     counter += 1
+#     try:
+#         guess = int(raw_input(">> "))
+#         if guess != random_number:
+#             if guess > 100 or guess < 1:
+#                 print "You idiot!! That number is not between 1 and 100. Try again."
+#             elif guess > random_number:
+#                 print "Your guess is too high. Try again."
+#             else:
+#                 print "Your number is too low. Try again."
 #         else:
-#             print "Your number is too low. Try again."
-#     else:
-#         print "Congratulations! You found my number in %d guesses!" % counter
-#         break
+#             print "Congratulations! You found my number in %d guesses!" % counter
+#             play_again = raw_input("Would you like to play again? (Yes/No) ")
+#             if play_again == "Yes":
+#                 print "%s, I'm thinking of a number between 1 and 100." % player_name
+#                 random_number = randint(1, 100)
+#                 print random_number
+#                 print "Guess what number I am thinking: "
+#                 counter = 0
+#                 continue
+#             else:
+#                 playing_game = False
+#     except ValueError:
+#         print "That is not a number!"
